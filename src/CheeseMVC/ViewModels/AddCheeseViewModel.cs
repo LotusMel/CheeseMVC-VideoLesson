0a0e3fb6957dbc81490d2a5c7af8bc09ff228af2
Melva Lang <mjlang74@gmail.com>
2020-04-26 21:07:05 -0500

    Completed ViewModels video lesson

---
+++ src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    // represents all of the data associated with the Add View - both displaying the form
+    // and processing the form.
+    
+    public class AddCheeseViewModel
+    {
+        [Required]
+        [Display(Name = "Cheese Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You must give your cheese a description.")]
+        public string Description { get; set; }
+    }
+}
